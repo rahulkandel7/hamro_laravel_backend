fedb46a9544f97e175ebe5b008d56c54a8fd5322
Sudip Parajuli <sudip.parajulee@gmail.com>
2022-10-09 13:27:36 +0545

    category finised

--- database/migrations/2022_10_09_062111_create_categories_table.php
+++ database/migrations/2022_10_09_062111_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('categoryName');
+            $table->integer('priority');
+            $table->string('photopath');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('V1')->middleware('auth:sanctum')->group(function(){
+    Route::apiResource('category',CategoryController::class);
 });
