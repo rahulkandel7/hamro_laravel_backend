87c6a25bef324c00dcc0676ec4c1b209373096f4
Sudip Parajuli <sudip.parajulee@gmail.com>
2022-10-11 17:04:42 +0545

    cat

--- database/migrations/2022_10_09_062111_create_categories_table.php
+++ database/migrations/2022_10_09_062111_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name');
+            $table->string('category_name'); 
             $table->integer('priority');
             $table->string('photopath');
             $table->timestamps();
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+ssdfsdafsdafasdfsdfsdfdsfsdfsfsdfsdssdsdfasfasdf
+
+
+sfsdfdsfsasdfsafsaf
+asdf
+asdfsddffasdf
+sdffffsaasdffadssfafasdf
+asdfasfsaffffasdfsafsdkfjkaslfks
+sjflslfasdjflsf
+asdklfjaslkjflskadf
+aaslkdjfkldsjlkjlkjasdkkjflkjjajklslsdfaf
+lskljjflllskdfjjalskkdjfjaljsdflasklfjsldjflsadjfljslfjsldjflsf
+lsjfklsjflsllfkjsdf
+lksjlfkajsd
+
+
+
+sf
+sad
+<fieldset><s>@selected(f
+    sdf
+    sdf
+    sd
+    fsd
+    f
+    sdf
+    sdf
+    sd
+    fsd
+    f)</s></fieldset>
