e40ef20ed43f4997d5ddff747861991f17ae0297
Sudip Parajuli <sudip.parajulee@gmail.com>
2022-10-11 17:35:41 +0545

    brandapi

---
+++ app/Http/Controllers/Api/V1/BrandController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BrandRequest;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Brand::all();
+        return response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BrandRequest $request)
+    {
+        $data = Brand::create($request->all());
+        return response()->json([
+            'message' => 'Brand Added Successfully',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BrandRequest $request, $id)
+    {
+        $brand = Brand::find($id);
+        $data = $brand->update($request->all());
+        return response()->json([
+            'message' => 'Brand Updated Successfully',
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Brand $brand)
+    {
+        $brand->delete();
+        return response()->json([
+            'message' => 'Brand Deleted Successfully',
+        ], 200);
+    }
+}
