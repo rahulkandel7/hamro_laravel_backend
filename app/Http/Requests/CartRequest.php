10d1c7465c773dd56d1acd54ac85c3db654551f6
Sudip Parajuli <sudip.parajulee@gmail.com>
2022-11-07 16:52:27 +0545

    already in cart

--- app/Http/Requests/CartRequest.php
+++ app/Http/Requests/CartRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CartRequest extends FormRequest
 {
@@ -26,11 +28,33 @@ class CartRequest extends FormRequest
         return [
             'product_id' => 'required',
             'quantity' => 'required',
-            'color' => 'nullable',
-            'size' => 'nullable',
+            'color' => 'required',
+            'size' => 'required',
             'price' => 'nullable',
             'ordered' => 'required',
             'status' => 'nullable',
+        ];
+
+        
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        $response = response()->json([
+            'message' => 'Invalid data send',
+            'details' => $errors->messages(),
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+
+    public function messages()
+    {
+        return[
+            'color.required' => 'Please Choose Color',
+            'size.required' => 'Please Choose Size',
         ];
     }
 }
